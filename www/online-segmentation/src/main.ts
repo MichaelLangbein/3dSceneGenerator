0b0bdcfe4f376bc543e722ad202d633adea920a5
michaellangbein <michael_langbein@hotmail.de>
2022-06-26 08:03:00 +0200

    moved away from ol. cannot do live analysis without a wms

--- www/online-segmentation/src/main.ts
+++ www/online-segmentation/src/main.ts
@@ -1,103 +1,82 @@
 import './style.css'
-import 'ol/ol.css';
-import { Map, View } from 'ol';
-import { Tile as TileLayer } from 'ol/layer';
-import WebGLTileLayer from 'ol/layer/WebGLTile';
-import { OSM, GeoTIFF } from 'ol/source';
-import { ScaleLine, Zoom, MousePosition, Attribution } from 'ol/control';
-import { getViewParas } from './utils';
-import RasterSource from 'ol/source/Raster';
-import ImageLayer from 'ol/layer/Image';
 import * as tf from '@tensorflow/tfjs';
+import { interpolateYlGn } from 'd3-scale-chromatic';
+import { rgb } from 'd3-color';
 
-
-
-// const cogUrl = "https://sentinel-cogs.s3.us-west-2.amazonaws.com/sentinel-s2-l2a-cogs/32/U/PU/2022/6/S2B_32UPU_20220612_0_L2A/TCI.tif";
-import cogUrl from '../data/s2/TCI.tif?url';
 import modelUrl from '../data/graphModel/model.json?url';
 
 
 
 
+const inputImage = document.getElementById('inputImage') as HTMLImageElement;
+const outputImage = document.getElementById('outputImage') as HTMLCanvasElement;
+const analyzeButton = document.getElementById('analyze') as HTMLButtonElement;
 
 const model = await tf.loadGraphModel(modelUrl);
-// const inputTensor = tf.zeros([1, 256, 256, 3]);
-// const outputTensor = model.predict(inputTensor) as tf.Tensor<tf.Rank.R4>;
-// const data = outputTensor.arraySync();
-// console.log(data);
 
 
-const viewParameters = getViewParas('EPSG:4326');
 
-const container = document.getElementById('map');
+async function analyzeImage() {
 
-const osmLayer = new TileLayer({
-    source: new OSM()
-})
+    console.log('Starting ...');
 
+    const outputData = tf.tidy(() => {
+        const inputTensor = tf.browser.fromPixels(inputImage);
+        const inputTensorF32 = tf.tensor(inputTensor.dataSync(), inputTensor.shape, 'float32');
+        const inputTensor4d = tf.reshape(inputTensorF32, [1, ... inputTensor.shape]);
+        const outputTensor = model.predict(inputTensor4d) as tf.Tensor<tf.Rank.R4>;
+        const outputData = outputTensor.arraySync();
+        return outputData[0];
+    });
+    
+    console.log('Creating output image ...')
 
-const s2Layer = new WebGLTileLayer({
-    source: new GeoTIFF({
-        sources: [{
-            url: cogUrl,
-        }],
-    })
-})
+    const w = inputImage.width;
+    const h = inputImage.height;
+    const imageData: Uint8ClampedArray = new Uint8ClampedArray(w * h * 4);
+    for (let r = 0; r < outputData.length; r++) {
+        const row = outputData[r];
+        for (let c = 0; c < row.length; c++) {
+            const pixelData = row[c];
+            const color = pixData2Color(pixelData);
+            imageData[(r*w + c)*4 + 0] = color[0];
+            imageData[(r*w + c)*4 + 1] = color[1];
+            imageData[(r*w + c)*4 + 2] = color[2];
+            imageData[(r*w + c)*4 + 3] = color[3];
+        }
+    }
+    const outputImageData = new ImageData(imageData, inputImage.width, inputImage.height);
+    outputImage.width = outputImageData.width;
+    outputImage.height = outputImageData.height;
+    const ctx = outputImage.getContext('2d');
+    ctx?.putImageData(outputImageData, 0, 0);
 
-function isImageData(input: any): input is ImageData {
-    return input.hasOwnProperty('data');
+    console.log('... done!');
+    console.log(outputImageData);
 }
 
-const segmentedLayer = new ImageLayer({
-    source: new RasterSource({
-        sources: [s2Layer.getSource()!],
-        operationType: 'image',
-        operation: function(inputImages: ImageData[] | number[][]): ImageData {
-            if (!isImageData(inputImages[0])) throw Error();
 
-            const inputImage: ImageData = inputImages[0];
+analyzeButton.addEventListener('click', analyzeImage);
 
-            const outputData = tf.tidy(() => {
-                const inputTensor = tf.browser.fromPixels(inputImage);
-                const inputTensor4d = tf.reshape(inputTensor, [1, ... inputTensor.shape]);
-                const outputTensor = model.predict(inputTensor4d) as tf.Tensor<tf.Rank.R4>;
-                const outputData = outputTensor.arraySync();
-                return outputData[0];
-            });
 
-            const classNr = 7;
 
-            const w = inputImage.width;
-            const h = inputImage.height;
-            const imageData: Uint8ClampedArray = new Uint8ClampedArray(w * h * 4);
-            for (let r = 0; r < outputData.length; r++) {
-                const row = outputData[r];
-                for (let c = 0; c < row.length; c++) {
-                    const pixelData = row[c];
-                    const classIntensity = pixelData[classNr];
-                    const color = Math.round(classIntensity * 255);
-                    imageData[r*w + c*4 + 0] = color;
-                    imageData[r*w + c*4 + 1] = color;
-                    imageData[r*w + c*4 + 2] = color;
-                    imageData[r*w + c*4 + 3] = color;
-                }
-            }
-            const outputImage = new ImageData(imageData, inputImage.width, inputImage.height);
-            return outputImage;
+
+
+function maxVal(data: number[]): number {
+    let maxI = 0;
+    let maxVal = -99999999;
+    for (let i = 0; i < data.length; i++) {
+        if (data[i] > maxVal) {
+            maxI = i;
+            maxVal = data[i];
         }
-    })
-});
+    }
+    return maxI;
+}
 
-const layers = [osmLayer, s2Layer];
-
-const view = new View(viewParameters);
-
-const controls = [new ScaleLine(), new Zoom(), new MousePosition(), new Attribution()];
-
-const map = new Map({
-    target: container!,
-    layers: layers,
-    view: view,
-    controls: controls,
-});
-
+function pixData2Color(pixelData: number[]): [number, number, number, number] {
+    const colorIndex = maxVal(pixelData);
+    const colorName = interpolateYlGn(colorIndex / 7);
+    const colorObj = rgb(colorName);
+    return [colorObj.r, colorObj.g, colorObj.b, 256];
+}
